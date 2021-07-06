b62f55c2ac29a268a6924d15c4ce7d370fd0c1e4
edersonfs <edersonfs@gmail.com>
2021-07-06 17:58:45 -0300

    capitulo 5

--- ProAgil.WebAPI/Controllers/ValuesController.cs
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -3,64 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ProAgil.WebAPI.Data;
-using ProAgil.WebAPI.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using ProAgil.Repository;
+using ProAgil.Domain;
 
 namespace ProAgil.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    public readonly ProAgilContext _context;
+
+    public ValuesController(ProAgilContext context)
     {
-        public readonly DataContext _context;
-        
-        public ValuesController(DataContext context)
-        {
-            this._context = context;
-        }
-
-        // GET api/values
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            try
-            {
-                var results = await _context.Eventos.ToListAsync();
-
-                return Ok(results);
-
-            }
-            catch (System.Exception)
-            {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");       
-            }            
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Evento>> Get(int id)
-        {
-            return await _context.Eventos.FirstOrDefaultAsync(x => x.EventoID == id);
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      this._context = context;
     }
+
+    // GET api/values
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+      try
+      {
+        var results = await _context.Eventos.ToListAsync();
+
+        return Ok(results);
+
+      }
+      catch (System.Exception)
+      {
+        return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+      }
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Evento>> Get(int id)
+    {
+      return await _context.Eventos.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
 }
