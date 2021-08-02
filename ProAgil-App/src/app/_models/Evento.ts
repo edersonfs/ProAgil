cca26c79f0932f0e757b7f9301f3f23073d3a7d0
edersonfs <edersonfs@gmail.com>
2021-08-02 14:51:13 -0300

    ajustes

---
+++ ProAgil-App/src/app/_models/Evento.ts
@@ -0,0 +1,13 @@
+export interface Evento {
+  id: number; 
+  local: string; 
+  dataEvento: Date; 
+  tema: string; 
+  qtdPessoas: number;         
+  imagemURL: string; 
+  telefone: string; 
+  email: string; 
+  lotes: Lote[];
+  redesSociais: RedeSocial[];
+  palestrantesEventos: Palestrante[];
+}
